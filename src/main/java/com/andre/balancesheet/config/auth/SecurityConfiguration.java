c8853b5bbc82e8392f60bd55ec134595509adfdb
André Abreu <andreagabreu1@gmail.com>
2024-01-17 13:56:50 -0300

    feat: add and configs authentication

---
+++ src/main/java/com/andre/balancesheet/config/auth/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.andre.balancesheet.config.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final JwtAuthenticationFilter jwtAuthnFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(req -> {
+                req.requestMatchers(HttpMethod.POST, "/api/v1/auth/**").permitAll();
+                req.anyRequest().authenticated();
+            })
+            .authenticationProvider(authenticationProvider)
+            .addFilterBefore(jwtAuthnFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
